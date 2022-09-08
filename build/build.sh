18adf2ce16c267edf919b7cec6a9546f7b136420
Alex Milowski <alex@microbyre.com>
2022-09-08 13:42:22 -0700

    removed build script

--- build/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf dist
-mkdir dist
-cp -r env/lib/python3.6/site-packages/* dist
-cp -r ../flask_serverless dist
-cp -r ../test_aws.py dist
-find dist -name __pycache__ -exec rm -rf {} \;
-rm lambda.zip
-cd dist; zip -r ../lambda.zip .
